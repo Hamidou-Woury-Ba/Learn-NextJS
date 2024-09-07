df69ea5500b4ee37922fd82630cf783b74bd9f92
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2024-09-07 12:37:24 +0000

    Daily UI

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,32 +1,44 @@
 import React, { cache } from 'react'
 
 interface User {
-    id: number
-    name: string
+  id: number,
+  name: string,
+  email: string
 }
 
 const UsersPage = async () => {
 
-    const res = await fetch(
-        'https://jsonplaceholder.typicode.com/users',
-        {
-            cache: 'no-store' // Force le navigateur à ne pas utiliser le cache pour cette requête 
-            // next : {
-            //     revalidate: 10, // Revalide la page toutes les 10 secondes
-            // }
-        },
-    )
-    const users : User[] = await res.json()
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/users',
+    {
+      cache: 'no-store' // Force le navigateur à ne pas utiliser le cache pour cette requête 
+      // next : {
+      //     revalidate: 10, // Revalide la page toutes les 10 secondes
+      // }
+    },
+  )
+  const users: User[] = await res.json()
 
   return (
     <>
       <h1 className='text-2xl font-bold'>Users</h1>
-      <p className='pt-5'> { new Date().toLocaleTimeString() } </p>
-      <ul className='py-10 px-10'>
-        {users.map(user => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <p className='pt-5'> {new Date().toLocaleTimeString()} </p>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
